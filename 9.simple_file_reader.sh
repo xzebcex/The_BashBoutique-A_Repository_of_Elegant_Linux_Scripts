916a52774e8a4b6193c34f4af41c1c6aae349220
Aman Pandit <amanpandit3027@gmail.com>
2023-04-22 13:50:18 +0530

    Create 9.simple_file_reader.sh
    Using the cat command, this Bash script reads the contents of a file named "sample.txt" and assigns it to a variable named "myvalue." The variable's contents are then printed to the terminal using the echo command.

---
+++ 9.simple_file_reader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash  
+myvalue='cat sample.txt' # This line reads the contents of a file called "sample.txt" and stores it in the variable "myvalue"
+
+echo "$myvalue" # This line prints the value of the "myvalue" variable to the terminal
